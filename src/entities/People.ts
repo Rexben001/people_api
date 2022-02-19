4482afacaa61091594e5e6b9ddd141a9736cbb6c
Rexben <rexben.rb@gmail.com>
2022-02-19 21:29:40 +0100

    feat: create entities

---
+++ src/entities/People.ts
@@ -0,0 +1,19 @@
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType()
+export class Person {
+  @Field()
+  name: string;
+  @Field()
+  height: string;
+  @Field()
+  mass: string;
+  @Field()
+  gender: string;
+  @Field()
+  homeworld: string;
+  @Field()
+  url: string;
+  @Field()
+  birth_year: string;
+}
